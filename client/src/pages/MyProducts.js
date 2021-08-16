3f63f850dda34892d8f80f2df6d3fe24cde664d6
Yazmin Tinoco <ytip30@gmail.com>
2021-08-15 21:35:54 -0500

    Create myProducts page

---
+++ client/src/pages/MyProducts.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+
+function MyProducts() {
+  const { data } = useQuery(QUERY_USER);
+  let user;
+
+  if (data) {
+    user = data.user;
+  }
+
+  return (
+    <>
+      <div className="container">
+        <Link style={{color: "black"}} to="/addProduct">← Add new product</Link>
+
+        {user ? (
+          <>
+            <h4>
+              Your products, {user.firstName} {user.lastName}
+            </h4>
+            {user.stockes.map((stock) => (
+              <div key={stock._id} className="my-2">
+                <div className="flex-row">
+                  {stock.products.map(({ _id, image, name, price }, index) => (
+                    <div key={index} className="card px-1 py-1">
+                      <Link to={`/products/${_id}`}>
+                        <img alt={name} src={`/images/${image}`} />
+                        <p style={{color: "black"}}>{name}</p>
+                      </Link>
+                      <div>
+                        <span>${price}</span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </>
+        ) : null}
+      </div>
+    </>
+  );
+}
+
+export default MyProducts;
